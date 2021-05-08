62f27129b2d052ab4e0c4f7d7206df2178144429
rahulrauth786 <83859768+rahulrauth786@users.noreply.github.com>
2021-05-08 17:29:02 -0400

    New File

---
+++ java-appium-app-browserstack/src/test/java/android/automation/Loginflow.java
@@ -0,0 +1,32 @@
+package android.automation;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import android.common.Combase;
+
+public class Loginflow extends Combase {
+	
+	
+	
+	@Test(priority = 1)
+	public static void Login() throws InterruptedException {
+		input("com.riviera.indifi.app:id/loginPhoneEditText", "8888833333");
+		click("com.riviera.indifi.app:id/loginContinueButton");
+		Thread.sleep(10000);
+	}
+	
+	@Test(priority = 2)
+	public static void OtpPage() throws InterruptedException {
+		
+		input("com.riviera.indifi.app:id/editOtpBox1", "1");
+		input("com.riviera.indifi.app:id/editOtpBox2", "2");
+		input("com.riviera.indifi.app:id/editOtpBox3", "3");
+		input("com.riviera.indifi.app:id/editOtpBox4", "4");
+		click("com.riviera.indifi.app:id/otpButtonVerify");
+		Thread.sleep(10000);
+		driver.quit();
+		
+	}
+
+}
